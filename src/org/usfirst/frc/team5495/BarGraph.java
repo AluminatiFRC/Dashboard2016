e076123c786a8025c2b4b042a169dba803f884e5
David Benedict <David Benedict>
2016-02-22 15:40:41 -0800

    added ability to change tolerance

--- src/org/usfirst/frc/team5495/BarGraph.java
+++ src/org/usfirst/frc/team5495/BarGraph.java
@@ -12,14 +12,15 @@ public class BarGraph extends JComponent {
 	private double lower;
 	private double upper;
 	private double target;
+	private double targetTolerance;
 	private double value;
 	private double interval;
+	private int valueMarker;
 	
-	public BarGraph (double lower, double upper) {
-		this(lower, upper, ((upper - lower + 1)/10.0));
+	public BarGraph (double lower, double upper, double interval, double tolerance) {
+		this(lower, upper, interval, tolerance, 6);
 	}
-		
-	public BarGraph (double lower, double upper, double interval) {
+	public BarGraph (double lower, double upper, double interval, double tolerance, int valueMarkerInPix) {
 		if (interval <= 0) {
 			throw new IllegalArgumentException("Interval must be a positive value above zero");
 		}
@@ -28,46 +29,50 @@ public class BarGraph extends JComponent {
 		this.upper = upper;
 		this.target = this.value = this.lower;
 		this.interval = interval;
+		this.targetTolerance = tolerance;
+		this.valueMarker = valueMarkerInPix;
 	}
 	
 	@Override
 	public void paintComponent(Graphics graphics) {
-	    int valueHeight = 6;
-		int targetHeight = 6;
 		int graphInset = 5;
 		int barWidth = getWidth() - graphInset * 2;
 		int barHeight = getHeight() - graphInset * 2;
 		double range = upper - lower;
-		double targetPercent = (target - lower) / (float)(range);
-		double valuePercent = (value - lower) / (float)(range);
-		int targetY = barHeight - (int) Math.round(targetPercent * barHeight) + graphInset - (targetHeight / 2);
-		int valueY = barHeight - (int) Math.round(valuePercent * barHeight) + graphInset - (valueHeight / 2);
+		double targetPercent = (target - lower) / range;
+		double valuePercent = (value - lower) / range;
+		int targetY = (int) (barHeight - (int) Math.round(targetPercent * barHeight) + graphInset - Math.round(targetTolerance / 2));
+		int valueY = barHeight - (int) Math.round(valuePercent * barHeight) + graphInset - (valueMarker / 2);
+		double u2PRatio = barHeight/range;
+		
 		
 		Graphics2D g = (Graphics2D) graphics;
-	
+		g.clipRect(graphInset, graphInset, barWidth + 1, barHeight + 1);
+		
 		// Draw the background/fill color
 		g.setColor(Color.BLUE);
 		g.fillRect(graphInset, graphInset, barWidth, barHeight);
 		
+		double tolerancePix = targetTolerance * u2PRatio;
+		
 		// Draw the target indicator
 		g.setColor(Color.RED);
-		g.fillRect(graphInset, targetY, barWidth, targetHeight);
+		g.fillRect(graphInset, targetY, barWidth, (int) tolerancePix);
 		
 		// Draw the value indicator
 		g.setColor(Color.GREEN);
-		g.fillRect(graphInset, valueY, barWidth, valueHeight);
+		g.fillRect(graphInset, valueY, barWidth, valueMarker);
 		
 		//calculate the tick marks
-		double p2URatio = barHeight/range;
-		double pix = interval*p2URatio;
+		double intervalPix = interval*u2PRatio;
 		
 		g.setColor(Color.ORANGE);
 		
 		Stroke tick = new BasicStroke(2.0f);
 		g.setStroke(tick);
 		int yStart = getHeight() - graphInset;
-		for (int i = 1;i <= (barHeight/pix); i++) {
-			g.drawLine(graphInset, yStart-(int) (i*pix), graphInset + (barWidth/2), yStart-(int) (i*pix));
+		for (int i = 1;i <= (barHeight/intervalPix); i++) {
+			g.drawLine(graphInset, yStart-(int) (i*intervalPix), graphInset + (barWidth/2), yStart-(int) (i*intervalPix));
 		}
 		
 		// Draw the border
@@ -75,6 +80,10 @@ public class BarGraph extends JComponent {
 		g.setStroke(border);
 		g.setColor(Color.BLACK);
 		g.drawRect(graphInset, graphInset, barWidth, barHeight);
+	}
+
+	public double getvalueMarker() {
+		return valueMarker;
 	}
 
 	public double getLower() {
